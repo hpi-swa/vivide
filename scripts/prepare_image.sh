a82c1b8d93e2cf4a6f46d729653e261cb7749903
Fabio Niephaus <code@fniephaus.com>
2015-09-27 10:41:03 +0200

    Fix and improve image preparation process

--- scripts/prepare_image.sh
+++ scripts/prepare_image.sh
@@ -6,12 +6,12 @@ function print_info {
     printf "\e[0;34m$1\e[0m\n"
 }
 
+# Check required arguments
+# ==============================================================================
 if [ "$TRAVIS_BRANCH" != "master" ]; then
     print_info "Nothing to do, because this is not the master branch"
     exit 0
-fi
-
-if [ -z "$PROJECT_HOME" ]; then
+elif [ -z "$PROJECT_HOME" ]; then
     print_info "\$PROJECT_HOME is not defined!"
     exit 1
 elif [ -z "$FILETREE_CI_HOME" ]; then
@@ -20,43 +20,41 @@ elif [ -z "$FILETREE_CI_HOME" ]; then
 elif [ -z "$SMALLTALK" ]; then
     print_info "\$SMALLTALK is not defined!"
     exit 1
+elif [ -z "$COG_VM" ]; then
+    print_error "\$COG_VM is not defined!"
+    exit 1
 fi
+# ==============================================================================
 
+# Set paths and files
+# ==============================================================================
 DEPLOY_PATH="$FILETREE_CI_HOME/deploy"
-
-BASE_PATH="$FILETREE_CI_HOME"
-CACHE_PATH="$BASE_PATH/cache"
-VM_PATH="$CACHE_PATH/vms"
-COG_VM_PATH="$VM_PATH/coglinux/bin/squeak"
-COG_VM_PARAM="-nosound -nodisplay"
 VIVIDE_IMAGE="Vivide-$SMALLTALK.image"
 VIVIDE_CHANGES="Vivide-$SMALLTALK.changes"
 DEPLOY_TARGET="https://www.hpi.uni-potsdam.de/hirschfeld/artefacts/vivide/"
+COG_VM_PARAM="-nosound -nodisplay"
+# ==============================================================================
 
-mkdir "$DEPLOY_PATH"
-cd "$DEPLOY_PATH"
+mkdir "$DEPLOY_PATH" && cd "$DEPLOY_PATH"
 
 if [ $SMALLTALK == "Squeak4.6" ]; then
     print_info "Downloading Squeak4.6 image..."
     wget http://ftp.squeak.org/4.6/Squeak4.6-15102.zip
     unzip Squeak4.6-15102.zip
-
     wget http://ftp.squeak.org/sources_files/SqueakV46.sources.gz
     gunzip SqueakV46.sources.gz
 elif [ $SMALLTALK == "Squeak5.0" ]; then
     print_info "Downloading Squeak5.0 image..."
     wget http://ftp.squeak.org/5.0/Squeak5.0-15113.zip
     unzip Squeak5.0-15113.zip
-
     wget http://ftp.squeak.org/sources_files/SqueakV50.sources.gz
     gunzip SqueakV50.sources.gz
 else
     print_info "Downloading SqueakTrunk image..."
-    wget http://build.squeak.org/job/SqueakTrunk/lastSuccessfulBuild/artifact/target/TrunkImage.zip
+    wget http://build.squeak.org/job/Trunk/default/lastSuccessfulBuild/artifact/target/TrunkImage.zip
     unzip TrunkImage.zip
-
-    wget http://ftp.squeak.org/sources_files/SqueakV41.sources.gz
-    gunzip SqueakV41.sources.gz
+    wget http://ftp.squeak.org/sources_files/SqueakV50.sources.gz
+    gunzip SqueakV50.sources.gz
 fi
 
 mv *.image "$VIVIDE_IMAGE"
@@ -64,7 +62,7 @@ mv *.changes "$VIVIDE_CHANGES"
 
 print_info "Preparing Vivide image from $SMALLTALK image..."
 EXIT_STATUS=0
-"$COG_VM_PATH" $COG_VM_PARAM "$VIVIDE_IMAGE" "$PROJECT_HOME/scripts/prepare_image.st" || EXIT_STATUS=$?
+"$COG_VM" $COG_VM_PARAM "$VIVIDE_IMAGE" "$PROJECT_HOME/scripts/prepare_image.st" || EXIT_STATUS=$?
 
 if [ $EXIT_STATUS -eq 0 ]; then
     print_info "Uploading $VIVIDE_IMAGE and $VIVIDE_CHANGES..."
